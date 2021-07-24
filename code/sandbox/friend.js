6388c767dd9bd55e5fe8f73a3b81775d0d1d1e0e
EM <61068739+Edit-Mr@users.noreply.github.com>
2021-07-24 21:37:08 +0800

    Update friend.js

--- code/sandbox/friend.js
+++ code/sandbox/friend.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./JSONHome/auth.json');
-const prefix = require('./JSONHome/prefix.json');
+const auth = require('./auth.json');
 
-client.login(auth.key);
+client.login(auth.token);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
 
 client.on('message', msg => {
     //前置判斷
@@ -22,49 +20,38 @@ client.on('message', msg => {
 
     //字串分析
     try {
-        let tempPrefix = '-1';
-        const prefixED = Object.keys(prefix); //前綴符號定義
-        prefixED.forEach(element => {
-            if (msg.content.substring(0, prefix[element].Value.length) === prefix[element].Value) {
-                tempPrefix = element;
-            }
-        });
+        const prefix = '!' //前綴符號定義
+        if (msg.content.substring(0, prefix.length) === prefix) //如果訊息的開頭~前綴字長度的訊息 = 前綴字
+        {
+            const cmd = msg.content.substring(prefix.length).split(' '); //以空白分割前綴以後的字串
 
-        //實作
-        switch (tempPrefix) {
-            case '0': //文字回應功能
-                const cmd = msg.content.substring(prefix[tempPrefix].Value.length).split(' '); //以空白分割前綴以後的字串
-                switch (cmd[0]) {
-                    case 'ping':
-                        msg.channel.send('pong');
-                        break;
-                    case '老婆':
-                        msg.reply('你沒有老婆!!');
-                        break;
-                    case 'myAvatar':
-                        const avatar = GetMyAvatar(msg);
-                        if (avatar.files) msg.channel.send(`${msg.author}`, avatar).catch(err => { console.log(err) });
-                        break;
-                }
-                break;
-            case '1': //音樂指令
-                const embed = new Discord.MessageEmbed()
-                  .setColor('#0099ff')
-                  .setTitle('Some title')
-                  .setURL('https://discord.js.org/')
-                  .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-                  .setDescription('Some description here')
-                  .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-                  .addField('Regular field title', 'Some value here')
-                  .addField('\u200B', '\u200B')
-                  .addField('Inline field title', 'Some value here', true)
-                  .addField('Inline field title', 'Some value here', true)
-                  .addField('Inline field title', 'Some value here', true)
-                  .setImage('https://i.imgur.com/wSTFkRM.png')
-                  .setTimestamp()
-                  .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-            msg.channel.send(embed);
-                break;
+            //功能實作
+            switch (cmd[0]) {
+                case 'ping':
+                    msg.channel.send('pong');
+                    break;
+                case '老婆':
+                    msg.reply('你沒有老婆!!');
+                    break;
+                case 'myAvatar':
+                    const embed = new Discord.MessageEmbed()
+                          .setColor('#0099ff')
+                          .setTitle('Some title')
+                          .setURL('https://discord.js.org/')
+                          .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+                          .setDescription('Some description here')
+                          .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+                          .addField('Regular field title', 'Some value here')
+                          .addField('\u200B', '\u200B')
+                          .addField('Inline field title', 'Some value here', true)
+                          .addField('Inline field title', 'Some value here', true)
+                          .addField('Inline field title', 'Some value here', true)
+                          .setImage('https://i.imgur.com/wSTFkRM.png')
+                          .setTimestamp()
+                          .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+                    msg.channel.send(embed);
+                    break;
+            }
         }
     } catch (err) {
         console.log('OnMessageError', err);
@@ -76,7 +63,7 @@ function GetMyAvatar(msg) {
     try {
         return {
             files: [{
-                attachment: msg.author.displayAvatarURL('png', true),
+                attachment: msg.author.displayAvatarURL,
                 name: 'avatar.jpg'
             }]
         };
