e63e6bb869b646b7388936b12cf06d4a4032113f
Elvis Mao <info@elvismao.com>
2025-05-21 13:47:37 +0800

    refactor: to astro WIP

---
+++ src/assets/intro/style.js
@@ -0,0 +1,23 @@
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.bottom < 0 || rect.top > window.innerHeight;
+}
+
+function addClassToVisibleElements() {
+    var aosElements = document.querySelectorAll(".aos, li");
+    aosElements.forEach(function (aosElement) {
+        if (!isElementInViewport(aosElement)) aosElement.classList.add("ed");
+        else aosElement.classList.remove("ed");
+    });
+}
+
+const showBody = () => {
+    document.body.style.opacity = 1;
+    // setTimeout(() => {
+    addClassToVisibleElements();
+    // }, 300);
+};
+addClassToVisibleElements();
+
+document.addEventListener("scroll", addClassToVisibleElements);
+window.addEventListener("pageshow", showBody);
